945e53f4c53b130a288d6e6f192a94d5bd44f36b
Viplow Srivastava <viplow.srivastava@indiamart.com>
2021-10-06 10:45:22 +0530

    Final Commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,89 @@
-import React from 'react';
-import HomeNav from './HomeNav';
+import React from "react";
+import HomeNav from "./HomeNav";
+import { Link } from "react-router-dom";
+import "./About.css";
+import { Animated } from "react-animated-css";
 
-const About = (props) => {
-    return (
-      <>
-        <HomeNav />
-        <div>About</div>
-      </>
-    );
-};
+function About() {
+  return (
+    <>
+      <HomeNav />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 about">
+            <h4>About Us</h4>
+
+            <div style={{ display: "flex" }}>
+              <div className="icon">
+                <Link to="https://www.facebook.com/">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </Link>
+              </div>
+              <div className="icon">
+                <Link to="https://www.twiter.com/">
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+                </Link>
+              </div>
+              <div className="icon">
+                <Link to="https://www.gmail.com/">
+                  <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                </Link>
+              </div>
+            </div>
+            <hr />
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <Animated animationIn="fadeInLeft" isVisible={true}>
+              <div
+                className="col-md-12 about-container"
+                style={{ textAlign: "center", alignItems: "center" }}
+              >
+                <h4>MAP STATISTICS DASHBOARD</h4>
+                <p>
+                  Created by Group - 2
+                </p>
+              </div>
+            </Animated>
+            <Animated animationIn="fadeInLeft" isVisible={true}>
+              <div
+                className="col-md-12 about-container"
+                style={{ textAlign: "center", alignItems: "center" }}
+              >
+                <h4>
+                  What are your sources ? How is the data gathered for this
+                  project ?
+                </h4>
+                <p>
+                  We are using disease.sh API. An open API for disease-related
+                  statistics.This API provides a big range of detailed
+                  information about multiple viruses.
+                </p>
+              </div>
+            </Animated>
+            <Animated animationIn="fadeInLeft" isVisible={true}>
+              <div
+                className="col-md-12 about-container"
+                style={{ textAlign: "center", alignItems: "center" }}
+              >
+                <h4>What is our aim ?</h4>
+                <p>
+                  Our aim is to built the COVID-19 global tracking app,
+                  aggregating the most comprehensive publicly available data set
+                  on the pandemic.We would like to provide resource to millions
+                  of users to track the novel coronavirus outbreak as it unfolds
+                  around the world.
+                </p>
+              </div>
+            </Animated>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default About;
